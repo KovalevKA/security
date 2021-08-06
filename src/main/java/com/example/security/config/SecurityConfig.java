77232bbb487cef69f8d46de6b555e38cb6ef4056
KovalevKA <kss47242@gmail.com>
2021-08-06 11:46:16 +0300

    auth with custom login form. with default db chema. user preauthorize
    no testing
    
    use preauthorize & custom login form & permit filter for access
    realize logout(/auth/logout), delete coocie & session

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.example.security.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -18,9 +19,24 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Bean
 	protected PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.withDefaultSchema()
+			.withUser(
+				User.withUsername("user")
+					.password(passwordEncoder().encode("user"))
+					.roles("USER")
+			);
 	}
 
 	@Override
@@ -45,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		;
 	}
 
-	@Bean
+	/*@Bean
 	@Override
 	public UserDetailsService userDetailsServiceBean() throws Exception {
 		return new InMemoryUserDetailsManager(
@@ -58,5 +74,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.roles("USER")
 				.build()
 		);
-	}
+	}*/
 }
